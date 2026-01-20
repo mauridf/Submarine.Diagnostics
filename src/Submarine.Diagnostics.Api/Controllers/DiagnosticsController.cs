5a59c0e3bd03b282260c4b1b7fd8a9e989bf06fc
mauridf <mauridf@gmail.com>
2026-01-20 10:32:33 -0300

    Adicionar arquivos de projeto.

---
+++ src/Submarine.Diagnostics.Api/Controllers/DiagnosticsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Submarine.Diagnostics.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiagnosticsController : ControllerBase
+    {
+    }
+}
